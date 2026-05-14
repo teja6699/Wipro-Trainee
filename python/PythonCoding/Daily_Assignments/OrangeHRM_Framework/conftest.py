7e87602bd4907bad394fcc5abe17e94c98b6bbbd
teja6699 <tejasaimanikanta123@gmail.com>
2026-05-14 10:23:41 +0530

    pandas

--- python/PythonCoding/Daily_Assignments/OrangeHRM_Framework/conftest.py
+++ python/PythonCoding/Daily_Assignments/OrangeHRM_Framework/conftest.py
@@ -1,21 +1,22 @@
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
 
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service
 
 
 @pytest.fixture()
 def driver():
 
-    driver = webdriver.Edge(
-        service=Service(
-            EdgeChromiumDriverManager().install()
-        )
+    service = Service(
+        executable_path=r"C:\softwares\edgedriver_win64\msedgedriver.exe"
     )
 
+    driver = webdriver.Edge(service=service)
+
     driver.maximize_window()
+
+    driver.implicitly_wait(10)
 
     driver.get(
         "https://opensource-demo.orangehrmlive.com/"
--- python/PythonCoding/Daily_Assignments/OrangeHRM_Framework/pages/login_page.py
+++ python/PythonCoding/Daily_Assignments/OrangeHRM_Framework/pages/login_page.py
@@ -1,23 +1,47 @@
 from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
 from pages.dashboard_page import DashboardPage
 
 
 class LoginPage:
 
     username_input = (By.NAME, "username")
+
     password_input = (By.NAME, "password")
-    login_button = (By.XPATH, "//button[@type='submit']")
+
+    login_button = (
+        By.XPATH,
+        "//button[@type='submit']"
+    )
 
     def __init__(self, driver):
 
         self.driver = driver
 
+        self.wait = WebDriverWait(driver, 20)
+
     def login(self, username, password):
 
-        self.driver.find_element(*self.username_input).send_keys(username)
+        self.wait.until(
+            EC.visibility_of_element_located(
+                self.username_input
+            )
+        )
 
-        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(
+            *self.username_input
+        ).send_keys(username)
 
-        self.driver.find_element(*self.login_button).click()
+        self.driver.find_element(
+            *self.password_input
+        ).send_keys(password)
 
-        return DashboardPage(self.driverpages)
+        self.driver.find_element(
+            *self.login_button
+        ).click()
+
+        return DashboardPage(self.driver)
